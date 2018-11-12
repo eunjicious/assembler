095dbae45ff618bebde668a02b8e81816a687ba0
KKM_VM <rkdals133@gmail.com>
2018-11-12 20:22:13 +0900

    check.c v1

--- check.c
+++ check.c
@@ -5,11 +5,20 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
+	char *sr;
+	char* dest;
+	char* str=args;	
+	sr=strtok(str,",");
+	str=strtok(NULL,",");
+	dest=strtok(str,",");
+
+	if(!strcmp(op,"movl"))
+		return 0;
+	if(strstr(dest,"$"))
+		return 0;
+	if(strstr(sr,"(")||strstr(sr,"0x"))
+		if(strstr(dest,"(")||strstr(dest,"0x"))
+			return 0;
 	
 	return 1;
 }
