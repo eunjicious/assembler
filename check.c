f59a3f0b9029249ed97a268a4b34ebd8abda25ef
KKM_VM <rkdals133@gmail.com>
2018-11-13 11:07:59 +0900

    Complete 2

--- check.c
+++ check.c
@@ -21,7 +21,7 @@ int is_valid(char *op, char *args)
 		dest_ch[j-i]=args[j];
 	if(dest_ch[0]=='$')
 		return 0;
-	if(sr_ch[0]=='('||sr_ch[0]=='0')
+	if(sr_ch[0]=='('||sr_ch[0]=='0'||sr_ch[0]=='-')
 		if(dest_ch[0]=='('||dest_ch[0]=='0')
 			return 0;
 
