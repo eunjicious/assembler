5a74c0c0c8d807612a8d0bbd9201dff4a93b8d2f
KKM_VM <rkdals133@gmail.com>
2018-11-13 10:30:44 +0900

    new V2

--- trans.c
+++ trans.c
@@ -19,7 +19,34 @@ int instr_trans(char *op, char *args, char* mcode)
 		else if(dest[0]=='0'&&dest[1]=='x')
 			strcpy(mcode,"a3");
 	}
-
-	
+	else if(sr[0]=='('||sr[strlen(sr)-1]==')')
+	{
+		strcpy(mcode,"8b");
+	}
+	else if(sr[0]=='0'&&sr[1]=='x')
+	{
+		if(strcmp(dest,"%eax"))
+		{
+			strcpy(mcode,"a1");
+		}
+	}
+	else if(sr[0]=='$'){
+		if(strcmp(dest,"%eax"))
+			strcpy(mcode,"b8");
+		else if(strcmp(dest,"%ecx"))
+			strcpy(mcode,"b9");
+		else if(strcmp(dest,"%edx"))
+			strcpy(mcode,"ba");
+		else if(strcmp(dest,"%ebx"))
+			strcpy(mcode,"bb");
+		else if(strcmp(dest,"%esp"))
+			strcpy(mcode,"bc");
+		else if(strcmp(dest,"%ebp"))
+			strcpy(mcode,"bd");
+		else if(strcmp(dest,"%esl"))
+			strcpy(mcode,"be");	
+		else if(strcmp(dest,"%edl"))
+			strcpy(mcode,"bf");
+	}
 	return 1;	
 }
