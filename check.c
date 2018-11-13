72dfb2844a81f674cf1778f8e149811f1d463b7b
KKM_VM <rkdals133@gmail.com>
2018-11-13 10:11:51 +0900

    new V1

--- check.c
+++ check.c
@@ -5,20 +5,25 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	char* sr_check=(char*)malloc(sizeof(char)*128);
-	char* dest_check=(char*)malloc(sizeof(char)*128);
-	char* str_check=args;	
-	sr_check=strtok(str_check,",");
-	str_check=strtok(NULL,",");
-	dest_check=strtok(str_check,",");
-
-	if(!strcmp(op,"movl"))
+	char sr_ch[128];
+	char dest_ch[128];
+	int i;
+	int j;
+	for(i=0;i<strlen(args);i++)
+	{
+		if(args[i]==','){
+			i++;
+			break;
+		}
+		sr_ch[i]=args[i];
+	}
+	for(j=i;j<strlen(args);j++)
+		dest_ch[j-i]=args[j];
+	if(dest_ch[0]=='$')
 		return 0;
-	if(dest_check[0]=='$')
-		return 0;
-	if(sr_check[0]=='('||sr_check[0]=='0')
-		if(dest_check[0]=='('||dest_check[0]=='0')
+	if(sr_ch[0]=='('||sr_ch[0]=='0')
+		if(dest_ch[0]=='('||dest_ch[0]=='0')
 			return 0;
-	
+
 	return 1;
 }
--- trans.c
+++ trans.c
@@ -7,51 +7,18 @@ int instr_trans(char *op, char *args, char* mcode)
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
-	char* sr=(char*)malloc(sizeof(char)*128);
-	char* dest=(char*)malloc(sizeof(char)*128);
+	char *sr,*dest;
 	char* str=args;
 	sr=strtok(str,",");
 	str=strtok(NULL,",");
-	dest=strtok(str,",");
-	if(sr[0]=='%'){
-		if(dest[0]=='%'){
+	dest=str;
+	printf("%s %s\n",sr,dest);
+	if(sr[0]=='%'&&sr[1]=='e'){
+		if(dest[0]=='%'&&dest[1]=='e')
 			strcpy(mcode,"89");
-		}
-		else if(dest[0]=='0')
-		{
+		else if(dest[0]=='0'&&dest[1]=='x')
 			strcpy(mcode,"a3");
-		}
 	}
-	else if(sr[0]=='('){
-		strcpy(mcode,"8b");
-	}
-	else if(sr[strlen(sr)-1]==')'){
-		strcpy(mcode,"8b");
-	}
-	else if(sr[0]=='0'){
-		if(strstr(dest,"%e"))
-			strcpy(mcode,"a1");
-	}
-	else if(sr[0]=='$'){
-		if(strcmp(dest,"%eax"))
-			strcpy(mcode,"b8");
-		else if(strcmp(dest,"%ecx"))
-			strcpy(mcode,"b9");
-		else if(strcmp(dest,"%edx"))
-			strcpy(mcode,"ba");
-		else if(strcmp(dest,"%ebx"))
-			strcpy(mcode,"bb");	
-		else if(strcmp(dest,"%esp"))
-			strcpy(mcode,"bc");
-		else if(strcmp(dest,"%ebp"))
-			strcpy(mcode,"bd");
-		else if(strcmp(dest,"%esl"))
-			strcpy(mcode,"be");
-		else if(strcmp(dest,"%edl"))
-			strcpy(mcode,"bf");
-	}
-	else
-		return 0;
 
 	
 	return 1;	
